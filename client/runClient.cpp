80014f1f01f4fa916a2ca9ea217c59e0ab7c9d29
祝方韦 <zfw15@mails.tsinghua.edu.cn>
2018-04-25 02:37:17 -0700

    structure

---
+++ client/runClient.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+#include "client.h"
+
+using namespace std;
+
+int main(){
+    Client cl;
+    cl.startClient();
+    return 0;
+}
---
+++ server/runServer.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+#include "server.h"
+
+using namespace std;
+
+int main(){
+    Server ser;
+    ser.startServer();
+    return 0;
+}
